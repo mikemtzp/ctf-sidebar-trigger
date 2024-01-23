4a196263285d7be547303cfe62e07652a7d13ca9
mikemtzp <mikemp8226@gmail.com>
2024-01-23 10:21:43 -0600

    add fiel height auto resizer

--- components/locations/Sidebar.tsx
+++ components/locations/Sidebar.tsx
@@ -1,9 +1,13 @@
 import { Paragraph, Button, Text } from '@contentful/f36-components';
 import { SidebarAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
+import { useEffect } from 'react';
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
+
+  // Ensure field resizes automatically
+  useEffect(() => sdk.window.startAutoResizer(), [sdk.window]);
 
   return (
     <>
