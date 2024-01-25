e32643269c5c7e776c30fa482695638da0776768
mikemtzp <mikemp8226@gmail.com>
2024-01-25 11:48:26 -0600

    Add logic to update entry when clickng Button

--- components/locations/Sidebar.tsx
+++ components/locations/Sidebar.tsx
@@ -1,11 +1,50 @@
 import { Paragraph, Button, Text } from '@contentful/f36-components';
 import { SidebarAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { createClient } from 'contentful-management';
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
   const [title, setTitle] = useState(false);
+  const [rawData, setRawData] = useState<any>();
+  const entryId = sdk.entry.getSys().id;
+
+  // Access Content Management API with credentials
+  const cma = useMemo(
+    () =>
+      createClient(
+        { apiAdapter: sdk.cmaAdapter },
+        {
+          type: 'plain',
+          defaults: {
+            environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
+            spaceId: sdk.ids.space,
+          },
+        }
+      ),
+    [
+      sdk.cmaAdapter,
+      sdk.ids.environmentAlias,
+      sdk.ids.environment,
+      sdk.ids.space,
+    ]
+  );
+
+  useEffect(() => {
+    // Get current entry data with entryId
+    const fetchData = async () => {
+      try {
+        const data = await cma.entry.get({ entryId });
+        setRawData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // Check if cma.entry and entryId are truthy
+    cma.entry && entryId && fetchData();
+  }, [cma.entry, entryId]);
 
   useEffect(() => {
     // Listen for changes on the title field
@@ -13,15 +52,25 @@ const Sidebar = () => {
       setTitle(value === undefined);
     });
 
-    // Ensure field resizes automatically
-    sdk.window.startAutoResizer();
-
     // Clean up the listener when the component is unmounted
     return () => detach();
-  }, [sdk.window, sdk.entry.fields.title]);
+  }, [sdk.entry.fields.title]);
+
+  useEffect(() => {
+    // Ensure field resizes automatically
+    sdk.window.startAutoResizer();
+  }, [sdk.window]);
 
   const handleClick = () => {
-    sdk.entry.save();
+    cma.entry
+      .update({ entryId }, rawData)
+      .then((entry) => {
+        console.log(entry);
+        console.log(`Entry ${entry.sys.id} updated.`);
+      })
+      .catch((error) => {
+        console.error('Error updating entry:', error);
+      });
   };
 
   return (
