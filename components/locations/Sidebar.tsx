b4632020f3b89b4280517bc7d34869816c6b0a8f
mikemtzp <mikemp8226@gmail.com>
2024-01-25 09:03:42 -0600

    activate button only when title field is true

--- components/locations/Sidebar.tsx
+++ components/locations/Sidebar.tsx
@@ -1,21 +1,43 @@
 import { Paragraph, Button, Text } from '@contentful/f36-components';
 import { SidebarAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
+  const [title, setTitle] = useState(false);
 
-  // Ensure field resizes automatically
-  useEffect(() => sdk.window.startAutoResizer(), [sdk.window]);
+  useEffect(() => {
+    // Listen for changes on the title field
+    const detach = sdk.entry.fields.title.onValueChanged((value) => {
+      setTitle(value === undefined);
+    });
+
+    // Ensure field resizes automatically
+    sdk.window.startAutoResizer();
+
+    // Clean up the listener when the component is unmounted
+    return () => detach();
+  }, [sdk.window, sdk.entry.fields.title]);
+
+  const handleClick = () => {
+    sdk.entry.save();
+  };
 
   return (
     <>
-      <Button variant='primary' isFullWidth>
-        Generate Build
+      <Button
+        variant='secondary'
+        isFullWidth
+        onClick={handleClick}
+        isDisabled={title}
+      >
+        Start Build
       </Button>
       <Paragraph marginBottom='none' marginTop='spacingS'>
-        <Text fontColor='gray700'>(AppId: {sdk.ids.app})</Text>
+        <Text fontColor='gray700'>
+          Click and wait 15 min to preview Draft articles.
+        </Text>
       </Paragraph>
     </>
   );
